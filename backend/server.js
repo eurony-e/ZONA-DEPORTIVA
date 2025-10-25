526e625e1f321722a07ab488cc987c43f09b376c
Juan Pablo Y. <euron9156@gmail.com>
2025-10-25 11:51:58 -0400

    feat(Frontend): versión funcional inicial con login, registro y panel de control

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 4000; // puerto del backend
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("Servidor backend de Zona Deportiva funcionando correctamente 🚀");
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
